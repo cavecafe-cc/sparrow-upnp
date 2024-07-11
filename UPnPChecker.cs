90ce6565eb0c615e35a5c0775d38c87aad4ed54e
thomas <758641+cavecafe@users.noreply.github.com>
2024-07-11 15:24:50 -0400

    initial commit

---
+++ UPnPChecker.cs
@@ -0,0 +1,77 @@
+using System.Net.Sockets;
+using SharpOpenNat;
+
+namespace Sparrow.UPnP;
+
+public class UPnPChecker(UPnPConfiguration upnp) {
+   private bool IsPortOpen(string host, int port, TimeSpan timeout) {
+      try {
+         using var client = new TcpClient();
+         var result = client.BeginConnect(host, port, null, null);
+         var success = result.AsyncWaitHandle.WaitOne(timeout);
+         if (!success) {
+            return false;
+         }
+
+         try {
+            client.EndConnect(result);
+         }
+         catch (SocketException se) {
+            Console.Error.WriteLine($"failed to close port {port}, err={se.Message}");
+            // ignored
+         }
+
+         return true;
+      }
+      catch (Exception e) {
+         Console.Error.WriteLine($"failed to check port {port}, err={e.Message}");
+         return false;
+      }
+   }
+
+   public bool CheckPortsOpened(string domain, IEnumerable<int> ports) {
+      return ports.All(port => IsPortOpen(domain, port, TimeSpan.FromSeconds(4)));
+   }
+
+   public async Task<bool> OpenPortAsync(string[] msg, int waitSeconds, CancellationToken cancel) {
+      if (!upnp.IsUpnpEnabled()) return false;
+
+      var result = false;
+      var openPorts = Task.Run(async () => {
+         try {
+            Console.WriteLine(string.Join("\n\t", msg));
+            Console.ReadKey(true);
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(waitSeconds));
+            var device = await NatDiscoverer.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+            var ip = await device.GetExternalIPAsync();
+            Console.WriteLine($"External IP: {ip}");
+            foreach (var map in upnp.PortMap!) {
+               var protocol = map.Protocol switch {
+                  "Tcp" => Protocol.Tcp,
+                  "Udp" => Protocol.Udp,
+                  _ => throw new NotSupportedException("Protocol not supported")
+               };
+               var mapping = new Mapping(protocol, map.Internal, map.External, map.Description);
+               await device.CreatePortMapAsync(mapping);
+               result = true;
+            }
+         }
+         catch (Exception e) {
+            await Console.Error.WriteLineAsync($"failed to open ports using UPnP, err={e.Message}");
+            result = false;
+         }
+
+         return result;
+      }, cancel);
+
+      try {
+         _ = await openPorts.WaitAsync(cancel);
+         return true;
+      }
+      catch (Exception e) {
+         await Console.Error.WriteLineAsync($"failed to open ports using UPnP, err={e.Message}");
+         return false;
+      }
+   }
+}
---
+++ UPnPConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Sparrow.UPnP;
+
+public class UPnPConfiguration {
+   public bool Enabled { get; init; }
+   public PortMap[]? PortMap { get; init; }
+
+   public bool IsUpnpEnabled() {
+      return this is { Enabled: true, PortMap.Length: > 0 };
+   }
+}
+
+public class PortMap {
+   public int Internal { get; init; }
+   public int External { get; init; }
+   public string Protocol { get; set; } = "Tcp";
+   public string Description { get; set; } = "UPnP";
+}
