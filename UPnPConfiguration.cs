27ad4ae6f84c1d32b76ab502650fabf425f104c4
thomas <758641+cavecafe@users.noreply.github.com>
2024-08-02 01:15:19 -0400

    set added

--- UPnPConfiguration.cs
+++ UPnPConfiguration.cs
@@ -1,8 +1,8 @@
 ﻿namespace Sparrow.UPnP;
 
 public class UPnPConfiguration {
-   public bool Enabled { get; init; }
-   public PortMap[]? PortMap { get; init; }
+   public bool Enabled { get; set; }
+   public PortMap[]? PortMap { get; set; }
 
    public bool IsUpnpEnabled() {
       return this is { Enabled: true, PortMap.Length: > 0 };
@@ -10,8 +10,8 @@ public class UPnPConfiguration {
 }
 
 public class PortMap {
-   public int Internal { get; init; }
-   public int External { get; init; }
+   public int Internal { get; set; }
+   public int External { get; set; }
    public string Protocol { get; set; } = "Tcp";
    public string Description { get; set; } = "UPnP";
 }
